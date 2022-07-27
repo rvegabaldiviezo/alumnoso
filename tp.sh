254f8322fafc5bf9aea4ae5ec2fecf76697e45df
rvegabaldiviezo <rvegabaldiviezo@frba.utn.edu.ar>
2022-07-27 05:28:53 -0300

    cambio eL TP.SH

--- tp.sh
+++ tp.sh
@@ -7,15 +7,14 @@ RUTA_INICIAL="/home/utnso"
 # 1. Instalar el TP
 sudo sh ${RUTA_SCRIPTS}/loggear.sh 1 "INGRESE SU PASSWORD, ES LA CONCATENACION DE DOS PALABRAS, PISTA: QUIEN HIZO EL MODULO MAS DIFICIL? Y COMO SE LLAMA ESE MODULO?"
 read password
-USER_TOKEN="$(curl --location --request GET 'https://martianeconomy-eval-prod.apigee.net/accesos/token?password={password}')"
+USER_TOKEN=$(curl --location --request GET "https://martianeconomy-eval-prod.apigee.net/accesos/token?password=${password}")
 cd $RUTA_SCRIPTS
 rm -rf $TPSO
-git clone https://${USER_TOKEN}@github.com/sisoputnfrba/tp-2022-1c-CobraKai.git
-
 MENSAJE="DESCARGO EL TP DE COBRAKAI"
 
 if [[ $USER_TOKEN != "error" ]]
 then
+    git clone "https://${USER_TOKEN}@github.com/sisoputnfrba/tp-2022-1c-CobraKai.git"
     sudo sh ${RUTA_SCRIPTS}/loggear.sh 1 "$MENSAJE"
 else
     sudo sh ${RUTA_SCRIPTS}/loggear.sh 3 " NO SE PUDO: $MENSAJE"
