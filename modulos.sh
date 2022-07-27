71add03614359ee5daa298d9869aea3446ef74d6
rvegabaldiviezo <rvegabaldiviezo@frba.utn.edu.ar>
2022-07-27 06:10:46 -0300

    cambios del tp.sh a

--- modulos.sh
+++ modulos.sh
@@ -6,24 +6,26 @@ TPSO=$2
 # 1 Compilar los modulos del TP: consola,kernel,memoria,cpu 
 modulo='memoria'
 cd "${RUTA_SCRIPTS}/${TPSO}/${modulo}/Debug"
-sudo rm -r "${modulo}"
-make all $modulo
+sudo make all $modulo
+pwd
 sudo sh "$RUTA_SCRIPTS/error.sh" "$?" "0" "COMPILO ${modulo}" $RUTA_SCRIPTS
 
-modulo='cpu'
-cd "${RUTA_INICIAL}/${TPSO}/${modulo}/Debug"
-sudo rm -r"${modulo}"
-make all $modulo 
-sudo sh "$RUTA_SCRIPTS/error.sh" "$?" "0" "COMPILO ${modulo}" $RUTA_SCRIPTS
+modulo2='cpu'
+cd "${RUTA_INICIAL}/${TPSO}/${modulo2}/Debug"
+pwd
+sudo make all $modulo2 
+sudo sh "$RUTA_SCRIPTS/error.sh" "$?" "0" "COMPILO ${modulo2}" $RUTA_SCRIPTS
 
-modulo='kernel'
-cd "${RUTA_INICIAL}/${TPSO}/${modulo}/Debug"
-sudo rm -r "${modulo}"
-make all $modulo 
-sudo sh "$RUTA_SCRIPTS/error.sh" "$?" "0" "COMPILO ${modulo}" $RUTA_SCRIPTS
+modulo3='kernel'
+cd "${RUTA_INICIAL}/${TPSO}/${modulo3}/Debug"
+sudo rm -r "${modulo3}"
+sudo make all $modulo3 
+pwd
+sudo sh "$RUTA_SCRIPTS/error.sh" "$?" "0" "COMPILO ${modulo3}" $RUTA_SCRIPTS
 
-modulo='consola'
-cd "${RUTA_INICIAL}/${TPSO}/${modulo}/Debug"
-sudo rm -r "${modulo}"
-make all $modulo 
-sudo sh "$RUTA_SCRIPTS/error.sh" "$?" "0" "COMPILO ${modulo}" $RUTA_SCRIPTS
+modulo4='consola'
+cd "${RUTA_INICIAL}/${TPSO}/${modulo4}/Debug"
+sudo rm -r "${modulo4}"
+sudo make all $modulo4
+pwd
+sudo sh "$RUTA_SCRIPTS/error.sh" "$?" "0" "COMPILO ${modulo4}" $RUTA_SCRIPTS
