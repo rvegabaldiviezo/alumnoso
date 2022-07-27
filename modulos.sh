c8b09b365254379512b42f81f1bcae27b1c68630
rvegabaldiviezo <rvegabaldiviezo@frba.utn.edu.ar>
2022-07-27 09:39:01 -0300

    corrijo la parte de make clean y all

--- modulos.sh
+++ modulos.sh
@@ -6,26 +6,28 @@ TPSO=$2
 # 1 Compilar los modulos del TP: consola,kernel,memoria,cpu 
 modulo='memoria'
 cd "${RUTA_SCRIPTS}/${TPSO}/${modulo}/Debug"
-sudo make all $modulo
+sudo make clean
+sudo make all
 pwd
 sudo sh "$RUTA_SCRIPTS/error.sh" "$?" "0" "COMPILO ${modulo}" $RUTA_SCRIPTS
 
 modulo2='cpu'
 cd "${RUTA_INICIAL}/${TPSO}/${modulo2}/Debug"
+sudo make clean
+sudo make all
 pwd
-sudo make all $modulo2 
 sudo sh "$RUTA_SCRIPTS/error.sh" "$?" "0" "COMPILO ${modulo2}" $RUTA_SCRIPTS
 
 modulo3='kernel'
 cd "${RUTA_INICIAL}/${TPSO}/${modulo3}/Debug"
-sudo rm -r "${modulo3}"
-sudo make all $modulo3 
+sudo make clean
+sudo make all
 pwd
 sudo sh "$RUTA_SCRIPTS/error.sh" "$?" "0" "COMPILO ${modulo3}" $RUTA_SCRIPTS
 
 modulo4='consola'
 cd "${RUTA_INICIAL}/${TPSO}/${modulo4}/Debug"
-sudo rm -r "${modulo4}"
-sudo make all $modulo4
+sudo make clean
+sudo make all
 pwd
 sudo sh "$RUTA_SCRIPTS/error.sh" "$?" "0" "COMPILO ${modulo4}" $RUTA_SCRIPTS
