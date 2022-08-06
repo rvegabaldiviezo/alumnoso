75e5055489ef0521badc513fa5c1fdd4b491a7a1
rvegabaldiviezo <rvegabaldiviezo@frba.utn.edu.ar>
2022-08-06 08:31:30 -0300

    cambiosss

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # Dar Permisos de Ejecucion a un script:  
-chmod a+x ./tp
+chmod a+x ./tp.sh
 # Copiar este comando en la carpeta de comandos
-sudo cp ./tp /usr/bin/
+sudo cp /home/utnso/alumnoso/tp.sh /usr/bin/
 
 cat /usr/bin/tp
